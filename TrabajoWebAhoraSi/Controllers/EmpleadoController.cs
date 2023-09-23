e7a92653aaf4a8f8f39e1f304df3647d36f70113
AleSanRu <alejandro2013lejandro@gmail.com>
2023-09-23 03:28:16 -0400

    Modificacion de la parte de controllore razor

---
+++ TrabajoWebAhoraSi/Controllers/EmpleadoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using TrabajoWebAhoraSi.Data;
+using TrabajoWebAhoraSi.Models;
+namespace TrabajoWebAhoraSi.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        public IActionResult MostrarEmpleados()
+        {
+            return View();
+        }
+    }
+}
